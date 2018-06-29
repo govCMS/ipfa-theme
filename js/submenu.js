3d813df9f7b9b8a02dddc497588214194d5a46a4
Petr Illek <petr@morpht.com>
2018-06-29 17:36:11 +0000

    ipfa-theme: Change the sticky element behavior.

--- js/submenu.js
+++ js/submenu.js
@@ -6,7 +6,7 @@
       // Add anchors to headings.
       $('#content').anchorific();
 
-      stickybits('.pane-node-field-submenu', {
+      stickybits('#header', {
         useFixed: true,
         noStyles: true
       });
@@ -16,22 +16,26 @@
         $('.field-name-field-submenu a').removeClass('active');
         $(this).addClass('active');
 
+        // Calculating scroll possition offset -x -y -z
+        // (x=admin navbar, y=headerHeight, z=extra distance)
         var target = $(this.hash);
+        var headerHeight = $('#header').outerHeight();
+
         if ($('body').hasClass('navbar-tray-open')) {
           $('html,body').stop().animate({
-            scrollTop: target.offset().top - 139
+            scrollTop: target.offset().top - 79 - headerHeight - 10
           }, 'linear');
         }
 
         else if ($('body').hasClass('navbar-horizontal')) {
           $('html,body').stop().animate({
-            scrollTop: target.offset().top - 99
+            scrollTop: target.offset().top - 39 - headerHeight - 10
           }, 'linear');
         }
 
         else {
           $('html,body').stop().animate({
-            scrollTop: target.offset().top - 60
+            scrollTop: target.offset().top - headerHeight - 10
           }, 'linear');
         }
 
@@ -40,9 +44,15 @@
   };
 
   $( window ).scroll(function() {
-    $('body').removeClass('submenu-is-sticky');
-    if ($('.pane-node-field-submenu').hasClass('js-is-sticky')) {
-      $('body').addClass('submenu-is-sticky');
+    var scrollpos = $(document).scrollTop();
+    var headerHeight = $('#header').outerHeight();
+    $('#page').css('padding-top', 0);
+
+    if(scrollpos > 0) {
+      $('#page').css('padding-top', headerHeight);
+    }
+    else {
+      $('#page').css('padding-top', 0);
     }
   });
 
